5bd006ee9dc8cc67a265047634f73b2cda96fc7a
Nuno Teles Silva <ntsilva@kpmg.com>
2018-08-31 09:53:51 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,19 @@ app.post('/todos', (req, res) => {
 
 });
 
+app.get('/todos', (req, res) => {
+
+  Todo.find().then((todos) => {
+    res.send({todos});
+    done();
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+});
+
+
+
 //GET /todos/23123ddqsd
 
 app.listen(3000, () => {
